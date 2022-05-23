dccb046a4b7f48544532d802b0bc6d8afb63dade
Hayden <contact@winisreallybored.com>
2022-05-23 11:13:56 -0700

    Create guildlevels.rb

---
+++ config/guildlevels.rb
@@ -0,0 +1,10 @@
+module DistoolsGuilds
+  # Guilds (friendguilds, supportguild)
+  # Looking for donor guilds? It's in Mongo.
+  # friendGuilds, array (int). additional perms linked to friendGuilds.
+  friendGuilds = []
+  # supportGuild, int. the support guild
+  supportGuild = 
+  # testGuild, int. guild where config/semi-sensitive options are outputted.
+  testGuild = 
+end
